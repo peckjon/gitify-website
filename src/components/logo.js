0412666b316e3da272ec2da663c6469b7e8cbe08
Emmanouil Konstantinidis <hello@manos.im>
2020-02-29 12:55:52 +0000

    Update logo.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -13,7 +13,7 @@ export const Logo = ({ isDark, onClick, style }) => (
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox="0 0 500 500"
-    role="image"
+    role="img"
     style={style}
   >
     <defs>
