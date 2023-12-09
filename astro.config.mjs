4a834860a71cbdc78572a66a349d4440227a029f
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-12-09 16:19:12 +0000

    feat: rollback to pure astro (#34)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind()],
 });
